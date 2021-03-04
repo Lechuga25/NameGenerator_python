fcf075cb1bd7b9622fb6efef80b4e7cc5735b28c
Eine Andere Mehr <einederemehr@gmail.com>
2021-03-03 21:38:42 -0600

    Design changes, readme changes

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from random import randint 
+from random import randint
 
 class Graphics():
     """
@@ -13,38 +13,50 @@ class Graphics():
     gen_name = ""
     def __init__(self, namegen):
         self.namegen = namegen
+        self.font = ("Helvetica", 20, )
+        self.bgcolor = "#1D2021"
+        self.fgcolor = "#DEDBCC"
 
         self.root = Tk()
+        self.root.configure(bg=self.bgcolor)
         self.frame = Frame(self.root)
+        self.frame.configure(bg=self.bgcolor)
         self.frame.pack()
 
         self.res = StringVar()
         self.res.set("Result")
 
-        self.numSyll_lbl = Label(self.frame, text="Number of Syllables", font=(30))
-        self.numSyll_lbl.grid(row=0, column=0)
+        self.numSyll_lbl = Label(self.frame, text="Number of Syllables: ", font=self.font, bg=self.bgcolor, fg=self.fgcolor)
+        self.numSyll_lbl.grid(row=0, column=0, sticky=W)
 
-        self.numSyll_txt = Entry(self.frame)
+        self.numSyll_txt = Entry(self.frame, bg="#2F3436", fg="#83A598", font=self.font, borderwidth=0, width=10, insertbackground=self.fgcolor)
         self.numSyll_txt.grid(row=0, column=1)
 
-        self.result_lbl = Label(self.frame, textvariable=self.res, font=40)
+        self.space_lbl = Label(self.frame, bg=self.bgcolor)
+        self.space_lbl.grid(row=1, column=0)
+
+        self.result_lbl = Label(self.frame, textvariable=self.res, font=self.font, bg=self.bgcolor, fg="#8EC07C")
         self.result_lbl.grid(row=2, column=1)
 
     def setName(self, num):
         try:
             self.numi = int(num)
-            self.res.set(self.namegen.generator(self.numi)) 
+            self.res.set(self.namegen.generator(self.numi).capitalize())
+            print("name generated: ", end="")
+            print(self.res.get())
         except Exception as ex:
             print(ex.__class__)
-            print(self.numSyll_txt.get())
+            # print(self.numSyll_txt.get())
             return
 
     def btn(self):
         self.gen = Button(self.frame, text="Generate", command= lambda num=self.numSyll_txt.get(): self.setName(self.numSyll_txt.get()))
-        self.gen.grid(row=2, column=0)
+        self.gen.configure(bg="#2F3436", fg="#83A598", font=self.font, borderwidth=0)
+        self.gen.grid(row=2, column=0, sticky=W)    
 
         
     def loop(self):
+        self.root.tk.call('tk', 'scaling', 2.0)
         self.root.mainloop()
 
     
@@ -57,37 +69,55 @@ class NameGen():
     of times it will mix things.
     
     every object in the file.txt needs to be separated by lines.
+    and you can use:
+    - 0 to especificate that it should only appear at the start of the name
+    - 1 to especificate that it should be at the middle of the name
+    - 2 to especificate that it should be at the end of the name
     """
-    lon = 0
+
     def __init__(self, path):
         self.path = path
+        self.lon = 0
 
+    def readfile(self, syllables):
+        with open(self.path, "r") as self.name:
+            self.rows = self.name.readlines()
+            for self.row in self.rows:
+                syllables.append(self.row.strip('\n'))
+        return syllables
 
     def generator(self , lon):
         self.syllables = []
         self.gen_name = ""
         self.i = 0
-        self.lon = lon
+        self.lon = lon - 1
+        self.syll = ""
+        self.syllables = self.readfile(self.syllables)
 
-        with open(self.path, "r") as self.name:
-            self.rows = self.name.readlines()
-            for self.row in self.rows:
-                self.syllables.append(self.row.strip('\n'))
-
-        while self.i <= (self.lon - 1):
+        while self.i <= (self.lon):
             self.r = randint(0, self.syllables.__len__()-1)
-            if (self.syllables[self.r].startswith("0") and self.i == 0):
-                self.gen_name += self.syllables[self.r].lstrip('0')
+            self.syll = self.syllables[self.r]
+
+            if (self.syll.startswith("0") and self.i == 0):
+                # self.gen_name += self.syll.lstrip('0')
+                self.gen_name += self.syll[1:]
+                
                 self.i += 1
-            elif (self.syllables[self.r].startswith("1") and self.i == self.lon):
-                self.gen_name += self.syllables[self.r].lstrip('1')
-                self.i += 1
-            elif (not(self.syllables[self.r].startswith("0")) and not(self.syllables[self.r].startswith("1"))):
-                self.gen_name += self.syllables[self.r]
+            
+            if (self.syll.startswith("1") and (self.i > 0 and self.i < self.lon)):
+                self.gen_name += self.syll[1:]
+                
                 self.i += 1
 
-        print("name generate:", end="")
-        print(self.gen_name)
+            if (self.syll.startswith("2") and self.i == self.lon):
+                self.gen_name += self.syll[1:]
+                
+                self.i += 1
+
+            if (not(self.syll.startswith("0")) and not(self.syll.startswith("1")) and not(self.syll.startswith("2"))):
+                self.gen_name += self.syll
+                self.i += 1
+
         return self.gen_name
 
 
@@ -96,5 +126,3 @@ if(__name__ == "__main__"):
     graphics = Graphics(namegen)
     graphics.btn()
     graphics.loop()
-
-
--- Readme.txt
+++ Readme.txt
@@ -23,3 +23,30 @@
     \_____\/  \:_\/  \________\/
 
 Release!!!!!
+
+first of all, you need python installed, then you open NameGenerator.py or NameGenNoconsole 
+then you choose the length of the name, 1 is a single syllable, 2 is more interesting, it
+will create names of two syllables, I do recomend the size of 3 or 2, they create better results.
+then you click the button "Generate" and boom, you have a random generated name.
+
+you can choose and configure the file "syllables.txt", this will have something like this:
+a
+e
+2mir
+1ta
+0li
+tre
+
+don't be scared, this are the syllables, you can delete or add the ones you like
+the 0, 1 and 2s are the way the program chooses the position of the syllable.
+
+0 - the syllable only appears at the beggining.
+1 - the syllable only appears at the middle.
+2 - the syllable only appears at the end.
+
+when there is no number at the beggining of the syllable the program
+places the syllable anywhere.
+
+
+
+
